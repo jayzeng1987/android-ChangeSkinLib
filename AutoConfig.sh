3c611a7972688d9a6d9533e5574315682126aeaa
jayzeng1987 <zengxiaojiang@192.168.1.103>
2017-01-17 21:06:09 +0800

    Add file 'AutoConfig.sh' on unix/mac platform.

---
+++ AutoConfig.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo -------------------- AutoConfig --------------------
+
+echo Git settting: assume unchanged files...
+
+git update-index --assume-unchanged project.properties
+git update-index --assume-unchanged .classpath
+git update-index --assume-unchanged .project
+
+echo ----------------------- End ------------------------
